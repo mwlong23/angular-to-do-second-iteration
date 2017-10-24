fde6028b683e4c99558aced209ab80035abe4992
Gennaro Anzalone and Mitchell Long <gennaro+mitchell@>
2017-10-24 14:48:57 -0700

    add ngIf directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   template:`
@@ -7,8 +8,22 @@ import { Component } from '@angular/core';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask()">Edit!</button></li>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
     </ul>
+    <hr>
+    <div *ngIf="selectedTask">
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
+      <h3>Edit Task</h3>
+      <label>Enter Task Description:</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      <button (click)="finishedEditing()">Done</button>
+    </div>
   </div>
   `
 })
@@ -22,11 +37,12 @@ export class AppComponent {
   tasks: Task[] = [
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on GitHub', 2)
-  ];
+    new Task('Add README file to last few Angular repos on GitHub', 2)];
+    selectedTask = null;
 
-  editTask(){
-    alert("You just requested to edit a Task!");
+
+  editTask(clickedTask){
+    this.selectedTask = clickedTask;
   }
 
   isDone(clickedTask: Task){
@@ -44,6 +60,9 @@ export class AppComponent {
     } else {
       return "bg-info";
     }
+  }
+  finishedEditing() {
+    this.selectedTask = null;
   }
 }
 
