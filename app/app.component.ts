1ed6b246130acb466668d7f1b8baaf898d928822
Mitch Long <meechllada@gmail.com>
2017-10-25 14:21:48 -0700

    begin to add template reference variables

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,31 +1,21 @@
+
 import { Component } from '@angular/core';
 import { Task } from './task.model';
 
-
 @Component({
   selector: 'app-root',
-  template:`
-  <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <task-list></task-list>
-    <hr>
-    <div *ngIf="selectedTask">
-      <h3>{{selectedTask.description}}</h3>
-      <p>Task Complete? {{selectedTask.done}}</p>
-      <h3>Edit Task</h3>
-      <label>Enter Task Description:</label>
-      <input [(ngModel)]="selectedTask.description">
-      <label>Enter Task Priority (1-3):</label>
-      <br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-      <button (click)="finishedEditing()">Done</button>
+  template: `
+    <div class="container">
+      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
+      <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+      <hr>
+      <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
+      <new-task></new-task>
     </div>
-  </div>
   `
 })
+
 
 export class AppComponent {
   currentFocus: string = 'Angular Homework';
@@ -34,10 +24,16 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
 
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)];
+    selectedTask = null;
 
-  editTask(clickedTask){
-    this.selectedTask = clickedTask;
-  }
+
+    editTask(clickedTask) {
+      this.selectedTask = clickedTask;
+    }
 
   finishedEditing() {
     this.selectedTask = null;
